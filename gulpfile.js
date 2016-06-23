862dbda7443df5744990f6e74b5ba54a295ef457
Lefteris K <lefteris.kokkonas@gmail.com>
2016-06-23 18:04:34 +0100

    updating oriental to v1.2 of SDK

--- gulpfile.js
+++ gulpfile.js
@@ -415,14 +415,14 @@ gulp.task('images', function() {
 });
 
 function filterStylesheets(path) {
+  // Temporary exclude the compiled booking bug css files if they exist
+  // until the new build process (as discussed) allows for their generation
   return (
     path.match(new RegExp('.css$'))
-    // Temporary exclude the compiled booking bug css files if they exist
-    // until the new build process (as discussed) allows for their generation
     &&
     !path.match(new RegExp('(bower_components\/bookingbug-angular-).+(\.css)'))
     &&
-    path.indexOf('boostrap.') !== -1
+    path.indexOf('boostrap.') == -1
   );
 }
 
